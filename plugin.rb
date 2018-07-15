a64941c9701b99bf700693b3d4f0c3ee7b32dd5c
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-07-15 23:29:50 +0530

    FEATURE: Site setting for a list of emojis to use. In order from left to right in the modal

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: procourse-community-feedback
+# about: Adds a popup for Discourse that asks users for feedback.
+# version: 0.1
+# authors: Procourse Team
+# url: https://github.com/procourse/procourse-community-feedback
+
+enabled_site_setting :procourse_community_feedback_enabled
+
+after_initialize do
+end
